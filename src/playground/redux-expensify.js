bdc2b09f8b0c3a8ccc31cc0398645a2f9b089b68
SnowSurfer <drinkaziu@gmail.com>
2019-01-17 15:27:03 -0500

    combine reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,48 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer 
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) =>{
+  switch (action.type) {
+    default: 
+      return state;
+  }
+};
+
+// Filters Reducer 
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default: 
+      return state; 
+  }
+};
+
+// Store Creation
+const store = createStore(
+  combineReducers({
+      expenses: expensesReducer, //root state name: reducer that manages it
+      filters: filtersReducer
+    })
+  );
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: 'NachoIsADog',
@@ -7,5 +50,11 @@ const demoState = {
     note: 'This is the first payment for this address',
     amount: 140000,
     createdAt: 0
-  }]
+  }], 
+  filters: {
+    text: 'rent',
+    sortBy: 'amount', //date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
 };
