13b51bd1c187cac0baa9e8a25c208633b7cbdcf1
SnowSurfer <drinkaziu@gmail.com>
2019-01-23 14:53:15 -0500

    organize redux

---
+++ src/reducers/expenses.js
@@ -0,0 +1,24 @@
+// Expenses Reducer 
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) =>{
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state, 
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(expense => {
+        return action.id !== expense.id
+      });
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        return expense.id === action.id ? { ...state, ...action.updates} : expense;
+      });
+    default: 
+      return state;
+  }
+};
+
+export default expensesReducer;
